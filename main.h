622e06caf1baaded4bff2d6c8959ca39fb857650
Mikasa12345667 <gmodera6@gmail.com>
2023-10-16 08:08:12 -0700

    created _printf.c

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int printf_char(va_list val);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_string(va_list val);
+int _strlenc(const char *str);
+int print_37(void);
+
+#endif
